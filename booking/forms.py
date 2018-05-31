43e5b8697faf392e010023c5d076d961676a49a5
adux <adriangarate@protonmail.com>
2018-05-31 16:25:20 +0200

    Details

--- booking/forms.py
+++ booking/forms.py
@@ -5,5 +5,5 @@ class BookingForm(forms.Form):
     address = forms.CharField(required=True)
     numero  = forms.CharField(required=True)
     email   = forms.EmailField(required=True)
-    option  = forms.CharField(required=True)
+    option  = forms.CharField(required=False)
     comment = forms.CharField(required=False)
