a1f66bad6d833b854d228e7e36754c2ed6354b40
adux <adriangarate@protonmail.com>
2018-07-25 15:16:46 +0200

    Admin Look for Booking

--- booking/admin.py
+++ booking/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import Booking
 # Register your models here.
 
+
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ('date','name','email','status','pay_till')
-    list_filter  = ('status','pay_till')
+    list_display = ('date', 'name', 'email', 'option', 'comment', 'status', 'pay_till')
+    list_filter = ('status', 'pay_till')
 
 
 admin.site.register(Booking, BookingAdmin)
