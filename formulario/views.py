9d03641bb7c294f9b47f4ac54e3edbec6989b2c2
adux <adriangarate@protonmail.com>
2018-11-16 16:56:25 +0100

    Auto Email - fix form save

--- formulario/views.py
+++ formulario/views.py
@@ -11,21 +11,19 @@ def fest_createview(request):
         print("post data")
         form = FestForm(request.POST)
         if form.is_valid():
-            instance = form.save(commit=False)
             obj = Fest.objects.create(
-                   name = form.cleaned_data.get('name'),
-                   address = form.cleaned_data.get('address'),
-                   numero = form.cleaned_data.get('numero'),
-                   email = form.cleaned_data.get('email'),
-                   option = form.cleaned_data.get('option'),
-                   allergies = form.cleaned_data.get('allergies')
+                name = form.cleaned_data.get('name'),
+                address = form.cleaned_data.get('address'),
+                numero = form.cleaned_data.get('numero'),
+                email = form.cleaned_data.get('email'),
+                option = form.cleaned_data.get('option'),
+                allergies = form.cleaned_data.get('allergies')
                 )
             subject = 'Winter Acro 2019'
-            message = "Hoi " + instance.name + "\r\n\r\nThanks for registering for the Winter Acro Festival 2019!\r\n\r\nLamas are little rebels, they are not good at doing automatic jobs. Definitly not as good as monkeys. Fly better though...\r\n\r\nAnyway, in the next 72 hours you will receive an email concerning your registration status! Thanks for your patience.\r\n\r\n\r\nBig Hug\r\nThe Lamas"
+            message = "Hoi Zäme\r\n\r\nThanks for registering for the Winter Acro Festival 2019!\r\n\r\nLamas are little rebels, they are not good at doing automatic jobs. Definitly not as good as monkeys. Fly better though...\r\n\r\nAnyway, in the next 72 hours you will receive an email concerning your registration status! Thanks for your patience.\r\n\r\n\r\nBig Hug\r\nThe Lamas"
             sender = 'notmonkeys@acrofestival.ch'
-            to = [instance.email]
+            to = obj.email
             send_mail(subject, message, sender, to)
-            instance.save()
             form.save()
         if form.errors:
             print(form.errors)
