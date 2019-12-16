662f0fff12050341725eb5e25b7cbb873d1f7514
adux <adriangarate@protonmail.com>
2019-12-16 15:40:51 +0100

    Magic

--- config/settings/base.py
+++ config/settings/base.py
@@ -70,7 +70,6 @@ LOCAL_APPS = [
     'acrofestival.users.apps.UsersConfig',
     'acrofestival.booking.apps.BookingConfig',
     'acrofestival.winteracro.apps.WinteracroConfig',
-    # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
