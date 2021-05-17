27f88c3d76e98d78f378851f4aca502b67d2c258
adux <adriangarate@protonmail.com>
2021-05-17 18:24:55 +0200

    Super early

--- acrofestival/static/dap/js/main.js
+++ acrofestival/static/dap/js/main.js
@@ -101,7 +101,7 @@ jQuery(document).ready(function($) {
 	//COUNTDOWN TIMER
 	var newYear = new Date();
     newYear = new Date(newYear.getFullYear() + 1, 1 - 1, 1);
-    $('#countdown').countdown({until: new Date(2021, 5-1, 18)}); // enter event day
+    $('#countdown').countdown({until: new Date(2021, 5-1, 19)}); // enter event day
 
     $('#removeCountdown').toggle(
         function() {
