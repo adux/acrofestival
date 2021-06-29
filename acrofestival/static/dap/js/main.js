15e871b70cdb67fdd66d02b87a1d9cd581cb9cfd
adux <adriangarate@protonmail.com>
2021-06-29 14:34:33 +0200

    Early Bird Change

--- acrofestival/static/dap/js/main.js
+++ acrofestival/static/dap/js/main.js
@@ -101,7 +101,7 @@ jQuery(document).ready(function($) {
 	//COUNTDOWN TIMER
 	var newYear = new Date();
     newYear = new Date(newYear.getFullYear() + 1, 1 - 1, 1);
-    $('#countdown').countdown({until: new Date(2021, 6-1, 29)}); // enter event day
+    $('#countdown').countdown({until: new Date(2021, 7-1, 1)}); // enter event day
 
     $('#removeCountdown').toggle(
         function() {
